45019f9ffba08c6efce4d83c6fe8b6f4faa6a41c
azizashari <aziz.ashari1@gmail.com>
2017-02-26 23:14:29 +0700

    First Commit

---
+++ app/src/main/java/com/android_examples/barchart_android_examplescom/config/Config.java
@@ -0,0 +1,17 @@
+package com.android_examples.barchart_android_examplescom.config;
+
+/**
+ * Created by Aziz on 23/02/2017.
+ */
+
+public class Config {
+
+    public static final String URL = "http://sipeja.pe.hu/";
+
+    //JSON for load status keuangan
+    public static final String JSON_ARRAY = "result";
+    public static final String KEY_BIAYA = "biaya";
+    public static final String KEY_LABS = "nama";
+    public static final String KEY_TAHUN = "tahun";
+}
+
